0625136b4a48fb95b58dbb7a2dbf53748bbcdbe1
John Hollander <john.hollander@stealthtrader.com>
2022-01-19 10:38:46 -0500

    Minor cleanups.

--- CSharp/MauiExample/MainPage.xaml.cs
+++ CSharp/MauiExample/MainPage.xaml.cs
@@ -31,11 +31,19 @@ namespace BridgeRock.MauiExample
 			_client.HeadroomUpdated += (s, e) => sgHeadroom.Value = e.Value;
 			_client.SentimentUpdated += (s, e) => s50t.UpdateSpectrum(e);
 
+<<<<<<< HEAD
 			_client.Connect("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
 							"eyJzdWIiOiJUZXN0QXBwIiwiaWF0IjoxNjMzMDEyMTUzLCJleHAiOjE2MzgyMz" +
 							"A0MDAsImF1ZCI6IjJXVWplb2JSWFJXOXBzTkRFY3hlMU1EOXd0ZGZkaDFDIn0." +
 							"xtykKWHxKwhopUkkyUm6eCa9qfQsGkhHEdAea9hdSz8");
 							
+=======
+            //_client.Connect("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+            //                "eyJzdWIiOiJUZXN0QXBwIiwiaWF0IjoxNjMzMDEyMTUzLCJleHAiOjE2MzgyMz" +
+            //                "A0MDAsImF1ZCI6IjJXVWplb2JSWFJXOXBzTkRFY3hlMU1EOXd0ZGZkaDFDIn0." +
+            //                "xtykKWHxKwhopUkkyUm6eCa9qfQsGkhHEdAea9hdSz8");
+
+>>>>>>> 2fa282c (Minor cleanups.)
             SubscribeSearch();
 			Subscribe(_symbol);
 		}
