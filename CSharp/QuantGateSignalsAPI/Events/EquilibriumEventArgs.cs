a1a935a83a7fba981888384dcb7a33a9139717dc
John Hollander <john.hollander@stealthtrader.com>
2024-10-18 14:06:41 -0400

    Spelling fix.

--- CSharp/QuantGateSignalsAPI/Events/EquilibriumEventArgs.cs
+++ CSharp/QuantGateSignalsAPI/Events/EquilibriumEventArgs.cs
@@ -76,7 +76,7 @@
         /// Whether the data used to generate this gauge value is potentially dirty 
         /// (values are missing) or stale (not the most recent data).
         /// </param>
-        /// <param name="error">Holds error information, if a subscription error occured.</param>
+        /// <param name="error">Holds error information, if a subscription error occurred.</param>
         internal EquilibriumEventArgs(string symbol, DataStream stream, DateTime timestamp, string compression,
                                       double equilibriumPrice, double gapSize, double lastPrice, double high, double low,
                                       double projectedPosition, double bias, bool isDirty, SubscriptionError error = null) :
