bd9ef484cc8e6c821afae0da55db2538d82a196c
John Hollander <john.hollander@stealthtrader.com>
2025-07-24 10:32:02 -0400

    Minor improvements to QuantGate Signals API.

--- CSharp/QuantGateSignalsAPI/Events/SubscriptionEventArgs.cs
+++ CSharp/QuantGateSignalsAPI/Events/SubscriptionEventArgs.cs
@@ -1,34 +1,33 @@
-﻿namespace QuantGate.API.Signals.Events
+﻿namespace QuantGate.API.Signals.Events;
+
+/// <summary>
+/// Base event args class for all subscriptions.
+/// </summary>
+public class SubscriptionEventArgs : EventArgs
 {
     /// <summary>
-    /// Base event args class for all subscriptions.
+    /// Reference object tied to this subscription.
     /// </summary>
-    public class SubscriptionEventArgs : EventArgs
+    public object Reference { get; protected internal set; }
+
+    /// <summary>
+    /// Holds error information, if a subscription error occured.
+    /// </summary>
+    public SubscriptionError Error { get; }
+
+    /// <summary>
+    /// Creates a new GaugeArgsBase instance.
+    /// </summary>        
+    /// <param name="reference">Reference object tied to this subscription.</param>        
+    /// <param name="error">Subscription error information, if an error occured.</param>
+    internal SubscriptionEventArgs(SubscriptionError error)
     {
-        /// <summary>
-        /// Reference object tied to this subscription.
-        /// </summary>
-        public object Reference { get; protected internal set; }
-
-        /// <summary>
-        /// Holds error information, if a subscription error occured.
-        /// </summary>
-        public SubscriptionError Error { get; }
-
-        /// <summary>
-        /// Creates a new GaugeArgsBase instance.
-        /// </summary>        
-        /// <param name="reference">Reference object tied to this subscription.</param>        
-        /// <param name="error">Subscription error information, if an error occured.</param>
-        internal SubscriptionEventArgs(SubscriptionError error)
-        {
-            Error = error;
-        }
-
-        /// <summary>
-        /// Creates a copy of this object.
-        /// </summary>
-        /// <returns>A copy of this object.</returns>
-        protected internal virtual object Clone() => MemberwiseClone();
+        Error = error;
     }
+
+    /// <summary>
+    /// Creates a copy of this object.
+    /// </summary>
+    /// <returns>A copy of this object.</returns>
+    protected internal virtual object Clone() => MemberwiseClone();
 }
